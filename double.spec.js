af715a73d1893a722edf6a1c9fce8efe67da0d4b
MatthiasLinder <matthias.linder@tptlive.ee>
2018-09-18 13:02:34 +0300

    Initial Fix of Doube.spec

--- double.spec.js
+++ double.spec.js
@@ -1,7 +1,7 @@
 const double = require("./double");
 
 it('double of 1 is 2', () => {
-    expect(double(1)).toBe(3);
+    expect(double(1)).toBe(2);
 });
 
 it('double of 3 is 6', () => {
@@ -10,6 +10,10 @@ it('double of 3 is 6', () => {
 
 it('double gets error with string input', () => {
     // TODO: write me!
+    expect(double('3')).toBe(6);
 });
 
 // TODO: add one more test case
+it('double of 4 is 8', () => {
+    expect(double(4)).toBe(8);
+});
